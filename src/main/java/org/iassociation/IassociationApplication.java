d274320f303567a2151f50edbd3bf3b8983e2e79
VonBurrito <hamza.amentag@hotmail.com>
2022-07-11 14:42:35 +0200

    IB-1: project structure

---
+++ src/main/java/org/iassociation/IassociationApplication.java
@@ -0,0 +1,13 @@
+package org.iassociation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IassociationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IassociationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/iassociation/IassociationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iassociation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IassociationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
